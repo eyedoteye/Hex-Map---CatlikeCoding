83e464aa6e123936ffb41c27379f2ce526aeab25
sigmasleep <sarkar.krishnan@gmail.com>
2017-09-03 00:33:47 -0400

    Init Commit

---
+++ HexMetrics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class HexMetrics : MonoBehaviour
+{
+  public const float radiusToVertex = 10f;
+  public const float radiusToEdge = radiusToVertex * 0.866025404f;
+
+  public static Vector3[] corners =
+  {
+    new Vector3(0f, 0f, radiusToVertex),
+    new Vector3(radiusToEdge, 0f, 0.5f * radiusToVertex),
+    new Vector3(radiusToEdge, 0f, -0.5f * radiusToVertex),
+    new Vector3(0f, 0f, -radiusToVertex),
+    new Vector3(-radiusToEdge, 0f, -0.5f * radiusToVertex),
+    new Vector3(-radiusToEdge, 0f, 0.5f * radiusToVertex)
+  }; 
+}
